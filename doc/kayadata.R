92d83976a5fc006c291e4bd518c62a5071476d59
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-12-19 19:03:31 -0600

    Fix up kayadata vignette to build under Travis-ci.

--- doc/kayadata.R
+++ doc/kayadata.R
@@ -1,6 +1,6 @@
 ## ----setup, include=FALSE-----------------------------------------------------
 knitr::opts_chunk$set(echo = FALSE)
-library(tidyverse)
+library(dplyr)
 library(kayadata)
 
 ## ----initialize, echo=TRUE, eval=FALSE----------------------------------------
